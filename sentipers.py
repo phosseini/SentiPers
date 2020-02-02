b3f13d47c90231f1024e3c9bddf1bb6cc6781bea
phosseini <pdr.hosseini@gmail.com>
2020-02-02 02:41:26 -0500

    adding read extra sentences method + updating jupyter demo

--- sentipers.py
+++ sentipers.py
@@ -17,9 +17,9 @@ class SentiPers:
         """
         df = pd.DataFrame(columns=self.sentences_columns)
 
-        def add_sentences(obj):
+        def add_sentences(sents):
             tmp_df = pd.DataFrame(columns=self.sentences_columns)
-            for sentence in obj:
+            for sentence in sents:
                 if len(polarity) == 0 or sentence.attrib["Value"] in polarity:
                     tmp_df = tmp_df.append([{'sid': sentence.attrib["ID"], 'text': sentence.text,
                                              'polarity': sentence.attrib["Value"],
@@ -42,4 +42,42 @@ class SentiPers:
                             df = df.append(add_sentences(child))
                 except Exception as e:
                     print("[log] details: " + str(e), full_path)
-        return df
+
+        # making sure rows have non-empty "text" and "polarity"
+        df = df.loc[(df["text"] != "") & (df["polarity"] != "")]
+
+        return df.drop_duplicates(subset=["text"], keep=False).reset_index()
+
+    def read_sentences_extra(self, polarity=[]):
+        """
+        reading sentences in extra folder format
+        :param polarity: filter for polarity. e.g. ["-1", "+1"]. possible value: ["-2", "-1", "0", "+1", "+2"]
+        :return:
+        """
+        df = pd.DataFrame(columns=self.sentences_columns)
+
+        for path in os.listdir(self.extra_path):
+            full_path = os.path.join(self.extra_path, path)
+            if os.path.isfile(full_path) and full_path.endswith(".xml"):
+                with open(full_path) as file:
+                    sentence = ""
+                    for line in file:
+                        # check if line is the label
+                        if "\t" not in line and all(char in line for char in ["[", "["]) and "[@@@]" not in line:
+                            label = line.replace('[', '').replace(']', '').replace('\n', '')
+
+                        else:
+                            # check if it's time to save sentence info
+                            if "[@@@]" in line and (len(polarity) == 0 or label in polarity):
+                                df = df.append([{'sid': '', 'text': sentence.strip(),
+                                                 'polarity': label,
+                                                 'file': full_path}], ignore_index=True)
+                                sentence = ""
+                                label = ""
+                            else:
+                                sentence += line.replace("\n", "").split("\t")[1] + " "
+
+        # making sure rows have non-empty "text" and "polarity"
+        df = df.loc[(df["text"] != "") & (df["polarity"] != "")]
+
+        return df.drop_duplicates(subset=["text"], keep=False).reset_index()
